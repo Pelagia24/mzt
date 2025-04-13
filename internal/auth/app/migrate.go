551a651299dd3eb29fed251033b95f8c54c40949
pelageia <chalykhpelagia@gmail.com>
2025-04-13 15:59:21 +0500

    add migration with initial admin

--- internal/auth/app/migrate.go
+++ internal/auth/app/migrate.go
@@ -1,8 +1,13 @@
 package app
 
 import (
+	"fmt"
 	"mzt/internal/auth"
 	"mzt/internal/auth/entity"
+	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Migrate(r *auth.UserRepo) {
@@ -18,5 +23,39 @@ func Migrate(r *auth.UserRepo) {
 	if err != nil {
 		panic(err)
 	}
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte("test1234"), bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
 
+	userId := uuid.New()
+
+	userEntity := &entity.User{
+		ID:         userId,
+		Email:      "test@test.test",
+		Role:       1,
+		PasswdHash: string(hashedPassword),
+	}
+
+	userData := &entity.UserData{
+		ID:          0,
+		UserID:      userId,
+		Name:        "test",
+		Birthdate:   time.Now(),
+		PhoneNumber: "+71111111111",
+		Telegram:    "@test123",
+	}
+
+	auth := &entity.Auth{
+		ID:     0,
+		UserID: userId,
+		Key:    "",
+	}
+
+	err = r.CreateUser(userEntity, userData, auth)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("migrated")
 }
